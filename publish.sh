d874da3673dce6e1ef8f0403be1fa2d4f258f8f5
Väinö Karppi <vaino.karppi@gmail.com>
2026-08-20 17:30:06 +0300

    Add Linux installer and update publish script

---
+++ publish.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+# ---------------- CONFIG ----------------
+path=./publish
+selfContained=true
+
+# Set which platforms to build
+buildWinX64=true
+buildLinuxX64=true
+buildLinuxArm64=true
+buildOsxX64=true
+
+# ---------------- FIND PROJECT FILE ----------------
+projFile=$(find . -maxdepth 1 -name "*.csproj" | head -n 1)
+if [ -z "$projFile" ]; then
+    echo "ERROR: No .csproj file found in the current directory."
+    exit 1
+fi
+echo "Using project file: $projFile"
+
+# ---------------- CLEAN ----------------
+rm -rf $path
+echo ""
+
+# ---------------- Windows x64 ----------------
+if [ "$buildWinX64" = true ]; then
+    echo "Building win-x64 version..."
+    dotnet publish "$projFile" --output $path/win-x64 --runtime win-x64 --self-contained $selfContained
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Windows Build failed!"
+        sleep 3
+        exit 1
+    fi
+    echo ""
+fi
+
+# ---------------- Linux ARM64 ----------------
+if [ "$buildLinuxArm64" = true ]; then
+    echo "Building linux-arm64 version..."
+    dotnet publish "$projFile" --output $path/linux-arm64 --runtime linux-arm64 --self-contained $selfContained
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Linux-arm64 Build failed!"
+        sleep 3
+        exit 1
+    fi
+    echo ""
+fi
+
+# ---------------- Linux x64 ----------------
+if [ "$buildLinuxX64" = true ]; then
+    echo "Building linux-x64 version..."
+    dotnet publish "$projFile" --output $path/linux-x64 --runtime linux-x64 --self-contained $selfContained
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Linux-x64 Build failed!"
+        sleep 3
+        exit 1
+    fi
+    echo ""
+fi
+
+echo "Build success!"
+echo ""
+sleep 1
+
+# ---------------- OSX x64 ----------------
+if [ "$buildOsxX64" = true ]; then
+    echo "Building osx-x64 version..."
+    dotnet publish "$projFile" --output $path/osx-x64 --runtime osx-x64 --self-contained $selfContained
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Osx-x64 Build failed!"
+        sleep 3
+        exit 1
+    fi
+    echo ""
+fi
+
+echo "Build success!"
+echo ""
+sleep 1
+
+# ---------------- Copy settings.json ----------------
+if [ -f "./settings.json" ]; then
+    echo "Copying settings.json..."
+    $buildWinX64 && cp ./settings.json $path/win-x64/settings.json
+    $buildLinuxArm64 && cp ./settings.json $path/linux-arm64/settings.json
+    $buildLinuxX64 && cp ./settings.json $path/linux-x64/settings.json
+else
+    echo "ERROR: settings.json not found, cannot continue!"
+    exit 1
+fi
+
+# ---------------- Copy Linux installer ----------------
+
+if [ ! -f "./install.sh" ]; then
+    echo "ERROR: install.sh not found!"
+    exit 1
+fi
+
+if [ "$buildLinuxArm64" = true ]; then
+    cp ./install.sh "$path/linux-arm64/install.sh"
+    chmod +x "$path/linux-arm64/install.sh"
+fi
+
+if [ "$buildLinuxX64" = true ]; then
+    cp ./install.sh "$path/linux-x64/install.sh"
+    chmod +x "$path/linux-x64/install.sh"
+fi
+
+echo ""
+echo "PUBLISH DONE!"
+cd $path
+echo "BUILD DIR: $PWD"
+echo ""
+read -p "Press enter to continue"
